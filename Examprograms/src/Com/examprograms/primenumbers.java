2c5abffc4c22c93861dedf04ac64772c247a1ea4
latha <latha@LATHA>
2023-04-25 12:39:51 +0530

    exampleprogram

---
+++ Examprograms/src/Com/examprograms/primenumbers.java
@@ -0,0 +1,42 @@
+package Com.examprograms;
+
+public class primenumbers {
+	public static void main(String[] args) {
+		
+	
+  int n=71;
+  int m=0;
+  for(int i=1;i<=n;i++) {
+	 
+		  if (n%i==0) {
+			  m++;
+			  
+		  }
+		  
+	  }
+	  if(m==2) {
+		  System.out.println("prime");
+	  
+	  }else {
+		   System.out.println("not prime");
+	  }
+	  
+// table
+	  
+	  
+	  int number = 5;
+	  for(int i =1; i<=10;i++) {
+		  System.out.println(number+ "*" +i+"="+number*i);
+		  
+	  }
+		  
+	
+}
+}
+
+	  
+	  
+	  
+  
+	  
+  

2cbec186009d16cb8b83ad40041249fe8966586e
latha <latha@LATHA>
2023-04-25 12:51:01 +0530

    maximumelement

---
+++ Maximumelement/src/Com/maximumelement/maximumelement.java
@@ -0,0 +1,21 @@
+package Com.maximumelement;
+
+public class maximumelement {
+  public static void main(String[] args) {
+	int []arr = new int[] {25,11,7,75,56};
+	
+	int max = arr[0];
+	for (int i = 0;i< arr.length;i++);{
+		
+	int i = 0;
+	if(arr[i]>max)
+			
+	max = arr[i];
+		
+			
+	}
+	
+	System.out.println("largest elements present in given array:"+max);
+	
+}
+}

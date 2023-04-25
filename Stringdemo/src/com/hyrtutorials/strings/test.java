e99e7a0ef81ff4f52fe978c26c7185769612ffdf
latha <latha@LATHA>
2023-04-25 13:20:52 +0530

    stringdemo

---
+++ Stringdemo/src/com/hyrtutorials/strings/test.java
@@ -0,0 +1,16 @@
+package com.hyrtutorials.strings;
+
+public class test {
+
+ public static void main(String[] args) {
+	    int i=10;
+		String s1= "yadagiri Reddy";
+		String emailid = "hyyadagirireddytutorials@gmail.com";
+		
+		
+	    String s2 = new String ("hello");
+		System.out.println(s1 + emailid);
+		System.out.println(s2);
+	    
+ }
+} 
